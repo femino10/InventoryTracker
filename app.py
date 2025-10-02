7c252642922f8c762e9b6f7b77550a6a4065a2cd
Felix Ndungu <ndungufelix20@gmail.com>
2025-10-02 19:38:17 +0300

    Added GET and POST endpoints for inventory items

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+items = [
+  {"id":1, "name": "Laptop", "quantity": 10},
+  {"id":2, "name": "Mouse", "quantity": 25},
+  {"id":3, "name": "Keyboard", "quantity": 15}
+]
 
 @app.route('/')
 def home():
     return "Welcome to Inventory Tracker!"
+
+@app.route('/items')
+def get_items():
+  return jsonify(items)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+  data = request.get_json()
+  new_item = {
+    "id": len(items) + 1,
+    "name": data["name"],
+    "quantity": data["quantity"]
+  }
+  items.append(new_item)
+  return jsonify(new_item), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
